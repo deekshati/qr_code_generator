f860e7b280d70e935e56b8e06345154c2c034e18
deekshat <deekshatiwari159@gmail.com>
2024-12-11 20:00:25 +0530

    Project finish

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 import inquirer from 'inquirer';
-var questions=
+import qr from "qr-image";
+import fs from "fs";
+
 inquirer
   .prompt([
+    {
     /* Pass your questions in here */
-    console.log("Can you generate omne qr image")
+    message:"Type in your URL: ",
+    name:"URL"
+    },
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
-    console.log("yes") 
+    const url=answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("URL.txt",url, (err)=> {
+        if (err) throw err;
+        console.log("The file has been saved!");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
-      console,log("couldn't be rendered in the current environment")
+      //console,log("couldn't be rendered in the current environment")
     } else {
-      console.log("Something else went wrong");
+      //console.log("Something else went wrong");
     }
   });
